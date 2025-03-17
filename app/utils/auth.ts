52533237a63ad121a8d9e89663968413a90ec277
Abdul100-han <abdulmuhd857@gmail.com>
2025-03-17 17:53:12 +0100

    implement authjs setup

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -9,7 +9,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             host: process.env.EMAIL_SERVER_HOST,
             port: process.env.EMAIL_SERVER_PORT,
             auth: {
-              user: process.env.EMAIL_SERVER_USER,
+              user: process.env.EMAIL_SERVER_USERfv,
               pass: process.env.EMAIL_SERVER_PASSWORD,
             },
           },
